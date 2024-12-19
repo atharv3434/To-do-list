d5df2fd243307a18b7c4175a5b4153f7f204c405
atharv3434 <128978076+atharv3434@users.noreply.github.com>
2024-12-19 13:01:03 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,79 +1,41 @@
-// import React,{useState,useEffect} from "react";
-// import { fetchData } from "./api";
+import React,{useState,useEffect} from "react";
+import { fetchData } from "./api";
 
-// const App=()=>{
-//   const[data,setData]=useState([]);
+const App=()=>{
+  const[data,setData]=useState([]);
 
-//   const[loading,setLoading]=useState(true);
+  const[loading,setLoading]=useState(true);
 
-//   const[error,setError]=useState(null);
+  const[error,setError]=useState(null);
 
-//   useEffect(()=>{
-//     const getData= async()=>{
-//       try{
-//         const result=await fetchData();
-//         setData(result)
-//       }
-//       catch(err){
-//         setError("Failed to Fetch Data");
-//       }
-//       finally{
-//         setLoading(false)
-//       }
-//     };
-//     getData();
-//   },[])
-//   if(loading) return <div>Loading .....</div>
-//   if(error) return <div>{error}</div>
-
-//   return(
-//     <div>
-//       <h1>Todo List</h1>
-//       <ul>
-//         {data.map((item)=>(
-//           <li key={item.id}>{item.title}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   )
-// }
-// export default App;
-
-import React, { useState, useEffect } from "react";
-import { fetchDatawithHeaders } from "./api";
-
-const App = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const getData = async () => {
-      try {
-        const result = await fetchDatawithHeaders();
-        setData(result);
-      } catch (err) {
+  useEffect(()=>{
+    const getData= async()=>{
+      try{
+        const result=await fetchData();
+        setData(result)
+      }
+      catch(err){
         setError("Failed to Fetch Data");
-      } finally {
-        setLoading(false);
+      }
+      finally{
+        setLoading(false)
       }
     };
     getData();
-  }, []);
+  },[])
+  if(loading) return <div>Loading .....</div>
+  if(error) return <div>{error}</div>
 
-  if (loading) return <div>Loading .....</div>;
-  if (error) return <div>{error}</div>;
-
-  return (
+  return(
     <div>
-      <h1>Pokemon List</h1>
+      <h1>Todo List</h1>
       <ul>
-        {data.map((item, index) => (
-          <li key={index}>{item.name}</li> // Display Pokémon names
+        {data.map((item)=>(
+          <li key={item.id}>{item.title}</li>
         ))}
       </ul>
     </div>
-  );
-};
-
+  )
+}
 export default App;
+
